a7f56ed78aa0b5b5d96985eb84d2a742bd07a9f1
satrioutamam <satrioutamam@gmail.com>
2021-12-23 15:46:35 +0700

    Changes 1

--- messageGen.js
+++ messageGen.js
@@ -4,28 +4,33 @@ const MixedMessages = {
     _b: ['I hate music', 'Music is to loud for me', 'I love silence', 'I do not want to hear drum sound'],
     _c: ['Follow me to museum', 'Love to stroll inside of history', 'I am curious about the story of man slaughter', 'Why dinosaur are eztinct?'],
     _d: ['Museum are boring', 'We couod do better than museum', 'let just drink alcohol and get drunk', 'Netflix and chill?'],
-    _qouotes(obj){
-        switch(obj){
+    _qouotes(obj) {
+        switch (obj) {
             case 0:
                 this._quote(this._a[Math.floor(Math.random() * this._a.length)] + ' - Rio');
-            break;
+                break;
             case 1:
                 this._quote(this._b[Math.floor(Math.random() * this._b.length)] + ' - Reza');
-            break;
-            case 2: 
+                break;
+            case 2:
                 this._quote(this._c[Math.floor(Math.random() * this._c.length)] + ' - Muli');
-            break;
+                break;
             case 3:
                 this._quote(this._d[Math.floor(Math.random() * this._d.length)] + ' - Tim');
-            break;
+                break;
         }
     },
-    _quote(obj){
+    _quote(obj) {
         console.log(obj)
     },
-    message(){
+    message() {
         this._qouotes(Math.floor(Math.random() * 3));
     }
 }
 
+MixedMessages.message();
+MixedMessages.message();
+MixedMessages.message();
+MixedMessages.message();
+MixedMessages.message();
 MixedMessages.message();
